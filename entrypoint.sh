2433415d3a4a152c15114598c824c8cb21ad8b0f
Maja Bojarska <majabojarska98@gmail.com>
2025-10-17 23:22:02 +0200

    feat: Require `BW_PORT` to be set in `entrypoint.sh` (#35)

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,15 +27,15 @@ fi
 if ! bw login --check; then
   bw config server "${BW_HOST}"
   if ! bw login --apikey; then
-    printf "\nFailed to login\n"
+    printf '\nFailed to login\n'
     exit 1
   fi
 fi
 
 if bw unlock --check; then
-  printf "Vault is already unlocked"
+  printf 'Vault is already unlocked'
 else
-  printf "\nUnlocking vault\n"
+  printf '\nUnlocking vault\n'
   # BW_PASSWORD is supposed to be the env name, not the value.
   BW_SESSION=$(bw unlock --passwordenv 'BW_PASSWORD' --raw)
   export BW_SESSION
@@ -43,13 +43,13 @@ else
   if ! bw unlock --check; then
     # Something went really wrong, we just logged in :/
     # Clean all state up, so that we can try again after a restart.
-    printf "\nFailed to unlock vault, removing full state\n"
+    printf '\nFailed to unlock vault, removing full state\n'
     rm -rf "${BITWARDENCLI_APPDATA_DIR:?}/"
     exit 1
   fi
 
   printf '\nSaving session key to %s\n' "${BW_SESSION_FILE}"
-  echo "${BW_SESSION}"> "${BW_SESSION_FILE}"
+  echo "${BW_SESSION}" > "${BW_SESSION_FILE}"
 fi
 
-bw serve --hostname 0.0.0.0 --port "${BW_PORT}"
+bw serve --hostname 0.0.0.0 --port "${BW_PORT:?}"
